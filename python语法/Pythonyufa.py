731c2f7ede8f43338dfdb978beec468ebac7dda7
kongf <1028564907@qq.com>
2023-04-25 00:04:50 +0800

    00

---
+++ python语法/Pythonyufa.py
@@ -0,0 +1,14 @@
+# 定义变量
+money = 50
+money-=10
+print("钱包：",money)
+print("类型",type(money))
+
+data = 10
+print(type(data))
+
+print(type(str(data)))
+
+for i in range(8,200) :
+    print(i)
+while True : print('Hello world')
